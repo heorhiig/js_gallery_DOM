683871ef3031005be007465ad919580f8f8420d6
Heorhii Churhuliia <gio4929189@gmail.com>
2024-03-28 02:41:12 +0000

    make build

---
+++ index.82c28979.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".list-item__link"),t=document.getElementById("largeImg");e.forEach(function(e){e.addEventListener("click",function(n){n.preventDefault();var r=e.href;t.src=r})});
+//# sourceMappingURL=index.82c28979.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-useless-return */ "use strict";
+const links = document.querySelectorAll(".list-item__link");
+const img = document.getElementById("largeImg");
+links.forEach((link)=>{
+    // eslint-disable-next-line no-shadow
+    link.addEventListener("click", (event)=>{
+        event.preventDefault();
+        const newImg = link.href;
+        img.src = newImg;
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
